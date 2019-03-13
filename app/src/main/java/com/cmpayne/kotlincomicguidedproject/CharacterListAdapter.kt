a4302a46cb9070f2875e25b6fe40b1c6c78144c2
Chance Payne <chance@lambdaschool.com>
2019-03-13 11:40:03 -0600

    finish recyclerview

--- app/src/main/java/com/cmpayne/kotlincomicguidedproject/CharacterListAdapter.kt
+++ app/src/main/java/com/cmpayne/kotlincomicguidedproject/CharacterListAdapter.kt
@@ -2,19 +2,32 @@ package com.cmpayne.kotlincomicguidedproject
 
 import android.app.Activity
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
-class CharacterListAdapter(val activity: Activity): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class CharacterListAdapter(val activity: Activity, val data: List<Character>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(view: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    class CharacterItemViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val characterNameView: TextView = view.findViewById(R.id.item_character_name)
+        val publisherNameView: TextView = view.findViewById(R.id.item_publisher_name)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflatedView = LayoutInflater.from(parent.context).inflate(R.layout.character_item_layout, parent, false)
+        return CharacterItemViewHolder(inflatedView)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return data.size
     }
 
     override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, index: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val element = data[index]
+        val characterHolder = viewHolder as CharacterItemViewHolder
+
+        characterHolder.characterNameView.text = element.name
+        characterHolder.publisherNameView.text = element.publisher?.name?:"Publisher Name"
     }
 }
