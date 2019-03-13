57ba22ccf027f2a7352f1c04a36ae9e44d48b306
Chance Payne <chance@lambdaschool.com>
2019-03-13 11:31:05 -0600

    start recyclerview

---
+++ app/src/main/java/com/cmpayne/kotlincomicguidedproject/CharacterListAdapter.kt
@@ -0,0 +1,20 @@
+package com.cmpayne.kotlincomicguidedproject
+
+import android.app.Activity
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+
+class CharacterListAdapter(val activity: Activity): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(view: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, index: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
