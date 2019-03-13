ccab5700a358c624d4dced9e61fffdc48b8add9b
Chance Payne <chance@lambdaschool.com>
2019-03-13 12:32:48 -0600

    add footer

--- app/src/main/java/com/cmpayne/kotlincomicguidedproject/CharacterListAdapter.kt
+++ app/src/main/java/com/cmpayne/kotlincomicguidedproject/CharacterListAdapter.kt
@@ -15,8 +15,15 @@ class CharacterListAdapter(val activity: Activity): RecyclerView.Adapter<Recycle
         val publisherNameView: TextView = view.findViewById(R.id.item_publisher_name)
     }
 
+    class FooterViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val countView: TextView = view.findViewById(R.id.count_view)
+    }
+
     companion object {
         const val ITEMS_PER_QUERY = 10
+
+        const val TYPE_FOOTER = 0
+        const val TYPE_ITEM = 1
     }
 
     val data = mutableListOf<Character>()
@@ -37,12 +44,27 @@ class CharacterListAdapter(val activity: Activity): RecyclerView.Adapter<Recycle
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val inflatedView = LayoutInflater.from(parent.context).inflate(R.layout.character_item_layout, parent, false)
-        return CharacterItemViewHolder(inflatedView)
+        return when(viewType) {
+            TYPE_FOOTER -> {
+                val inflatedView = LayoutInflater.from(parent.context).inflate(R.layout.footer_layout, parent, false)
+                FooterViewHolder(inflatedView)
+            }
+            else -> {
+                val inflatedView = LayoutInflater.from(parent.context).inflate(R.layout.character_item_layout, parent, false)
+                CharacterItemViewHolder(inflatedView)
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when {
+            position >= data.size -> TYPE_FOOTER
+            else -> TYPE_ITEM
+        }
     }
 
     override fun getItemCount(): Int {
-        return data.size
+        return data.size + 1
     }
 
     override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, index: Int) {
