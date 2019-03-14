22a086eca800d4c8513ac0648f48346917037f91
Chance Payne <chance@lambdaschool.com>
2019-03-14 10:54:41 -0600

    add suspend to dao and networkadapter

--- app/src/main/java/com/cmpayne/kotlincomicguidedproject/ComicDao.kt
+++ app/src/main/java/com/cmpayne/kotlincomicguidedproject/ComicDao.kt
@@ -1,10 +1,12 @@
 package com.cmpayne.kotlincomicguidedproject
 
+import android.support.annotation.WorkerThread
 import kotlinx.serialization.json.Json
 
 object ComicDao {
 
-    const val CHARACTERS_URL = "https://comicvine.gamespot.com/api/characters?api_key=789825a0e6665829732b5a594a79157f246c0ebe&format=json"
+    const val CHARACTERS_URL =
+        "https://comicvine.gamespot.com/api/characters?api_key=789825a0e6665829732b5a594a79157f246c0ebe&format=json"
 //    const val CHARACTERS_URL = "https://api.comicvine.com/characters?api_key=789825a0e6665829732b5a594a79157f246c0ebe&format=json&limit=10"
 
     interface CharactersCallback {
@@ -12,14 +14,26 @@ object ComicDao {
     }
 
     fun getCharacters(callback: CharactersCallback, offset: Int = 0, limit: Int = 10) {
-        NetworkAdapter.httpGetRequest("$CHARACTERS_URL&limit=$limit&offset=$offset", object: NetworkAdapter.NetworkCallback {
-            override fun returnResult(success: Boolean?, result: String) {
-                if(success == true) {
-                    val characterList = Json.parse(CharacterList.serializer(), result)
+        NetworkAdapter.httpGetRequest(
+            "$CHARACTERS_URL&limit=$limit&offset=$offset",
+            object : NetworkAdapter.NetworkCallback {
+                override fun returnResult(success: Boolean?, result: String) {
+                    if (success == true) {
+                        val characterList = Json.parse(CharacterList.serializer(), result)
 
-                    callback.callback(characterList.results?: listOf())
+                        callback.callback(characterList.results ?: listOf())
+                    }
                 }
-            }
-        })
+            })
+    }
+
+    @WorkerThread
+    suspend fun getCharacters(offset: Int = 0, limit: Int = 10): List<Character> {
+        val (success, result) = NetworkAdapter.httpGetRequest("$CHARACTERS_URL&limit=$limit&offset=$offset")
+        var characterList: CharacterList? = null
+        if (success) {
+            characterList = Json.parse(CharacterList.serializer(), result)
+        }
+        return characterList?.results ?: listOf<Character>()
     }
 }
--- app/src/main/java/com/cmpayne/kotlincomicguidedproject/NetworkAdapter.kt
+++ app/src/main/java/com/cmpayne/kotlincomicguidedproject/NetworkAdapter.kt
@@ -1,5 +1,6 @@
 package com.cmpayne.kotlincomicguidedproject
 
+import android.support.annotation.WorkerThread
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStream
@@ -11,6 +12,54 @@ import java.net.URL
 object NetworkAdapter {
     interface NetworkCallback {
         fun returnResult(success: Boolean?, result: String)
+    }
+
+    @WorkerThread
+    suspend fun httpGetRequest(urlString: String): Pair<Boolean, String> {
+        var result = ""
+        var success = false
+        var connection: HttpURLConnection? = null
+        var stream: InputStream? = null
+        try {
+            val url = URL(urlString)
+            connection = url.openConnection() as HttpURLConnection
+            connection.connect()
+
+            val responseCode = connection.responseCode
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                stream = connection.inputStream
+                if (stream != null) {
+                    val reader = BufferedReader(InputStreamReader(stream))
+                    val builder = StringBuilder()
+                    var line: String? = reader.readLine()
+                    while (line != null) {
+                        builder.append(line)
+                        line = reader.readLine()
+                    }
+                    result = builder.toString()
+                    success = true
+                }
+            } else {
+                result = responseCode.toString()
+            }
+        } catch (e: MalformedURLException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            connection?.disconnect()
+
+            if (stream != null) {
+                try {
+                    stream.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+            }
+
+            return success to result
+        }
     }
 
     fun httpGetRequest(urlString: String, callback: NetworkCallback) {
@@ -61,7 +110,5 @@ object NetworkAdapter {
             }
             //                return result;
         }).start()
-
-
     }
 }
